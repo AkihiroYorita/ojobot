a228d4b97d160c615ee6643e4fa8009cfec9b194
音石朋恵 <tomoe_otoishi@Tomoe-Otoishi.local>
2016-03-13 17:55:59 +0900

    correction

--- autofollowback.rb
+++ autofollowback.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 keys = YAML.load_file('./config.yml')
 
-client_rest = Twitter::REST::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = keys["api_key"]
   config.consumer_secret = keys["api_secret"]
   config.access_token = keys["access_token"]
